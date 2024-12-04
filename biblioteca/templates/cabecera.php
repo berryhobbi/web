bbc90cc200887e7631cf66245cfc44153e8ea99b
berryhobbi <a22brenda.garciaa6@cbtis187.edu.mx>
2024-12-04 14:18:45 -0600

    Add files via upload

---
+++ biblioteca/templates/cabecera.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Biblioteca Escolar Cbtis No. 187</title>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+</head>
+<body>
+    <div class="container-fluid bg-warning"><!--sirve para agrupar elementos relacionados -->
+
+        <div class="container text-center">
+            <div class="row">
+            <div class="col">
+     
+            <img src="img/logo_degeti.png" alt="" style="width:100px; height:100px;">
+</div>
+<div class="col">
+<strong>Biblioteca Escolar CBtis No. 187</strong><!-- sirve para indicar que el texto tiene mayor importancia que el resto del texto-->
+</div>
+<div class="col">
+
+<img src="img/bandera sin fondo.png" alt="" style="width:100px; height:100px;">
+ 
+</div>
+</div>
+</div>
+</div>
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #6f42c1"><!--sirve para definir una sección de navegación en un documento web-->
+
+
+    <ul class="nav navbar-nav"><!--se utiliza para presentar una lista de elementos sin orden especifico-->
+
+        <li class="nav-item dropdown"><!--se utiliza para representar un elemento dentro de una lista-->
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Libros
+</a>
+<ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="registro_libros.php">Registrar nuevo libro</a></li>
+    <li><a class="dropdown-item" href="libros_registrados.php">Mostrar libros registrados</a></li>
+</ul>
+</li>
+<li class="nav-item dropdown">
+    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Usuarios
+</a>
+<ul class="dropdown-menu">
+    <li><a class="dropdown-item" href="registro_usuarios_alumnos.php">Registrar nuevo alumno</a></li>
+    <li><a class="dropdown-item" href="registro_usuarios_docentes.php">Registrar nuevo docente</a></li>
+    <li><a class="dropdown-item" href="alumnos_registrados.php">Mostrar alumnos registrados</a></li>
+    <li><a class="dropdown-item" href="docentes_registrados.php">Mostrar Docentes registrados</a></li>
+</ul>
+</li>
+
+
+
+
+<li class="nav-item dropdown">
+<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+    Movimientos
+</a>
+<ul class="dropdown-menu">
+<li><a class="dropdown-item" href="prestamos.php">Realizar prestamo</a></li>
+<li><a class="dropdown-item" href="devoluciones.php">Devolver libro</a></li>
+<li><a class="dropdown-item" href="mostrar_prestamos.php">Mostrar prestamos</a></li>
+</ul>
+</li>
+
+<li class="nav-item">
+    <a class="nav-link" href="acerca_de.php">Acerca de</a>
+</li>
+</div>
+</div>
+</ul>
+</nav>
+<div class="container">
+    <br>
+    <div class="row">
